efe3c6996c92acce2f4179f899d736ae99b236c0
kennataddese6 <kennataddese6@gmail.com>
2025-02-28 23:15:23 +0300

    make leads mobile responsive

--- components/ui/lead.tsx
+++ components/ui/lead.tsx
@@ -1,15 +1,15 @@
 const Lead = () => {
   return (
-    <div className="bg-blue-800 bg-[url(/leads-image.jpg)] h-5/6 bg-no-repeat bg-cover mt-12 flex items-center">
-      <div className="w-1/3 md:mx-20">
-        <h1 className="text-4xl  text-[#17323D] font-semibold">
+    <div className=" bg-[url(/leads-image.jpg)] h-5/6 bg-no-repeat bg-cover mt-12 flex items-center">
+      <div className="md:w-1/3 mx-4 md:mx-20">
+        <h1 className="text-4xl  text-[#17323D] font-semibold text-center md:text-start">
           Leading health care Providers
         </h1>
-        <p className="my-4">
+        <p className="my-4 text-center md:text-start ">
           We provide the best choices for you. Adjust it to your health needs
           and make sure you get them whenever you need them.
         </p>
-        <button className="py-2 px-8 border-2 border-[#17323D] text-[#17323D] font-semibold">
+        <button className="py-2 px-8 border-2 border-[#17323D] text-[#17323D] font-semibold block mx-auto md:inline">
           View Products
         </button>
       </div>
