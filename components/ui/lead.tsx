b77c2913f0a8dba2ed6db253fb39bc3a4745fb8b
kennataddese6 <kennataddese6@gmail.com>
2025-02-28 23:32:14 +0300

    add motion to leads

--- components/ui/lead.tsx
+++ components/ui/lead.tsx
@@ -1,17 +1,34 @@
+"use client"
+import { motion } from "motion/react"
 const Lead = () => {
   return (
     <div className=" bg-[url(/leads-image.jpg)] h-5/6 bg-no-repeat bg-cover mt-24 flex items-center">
       <div className="md:w-1/3 mx-4 md:mx-20">
-        <h1 className="text-4xl  text-[#17323D] font-semibold text-center md:text-start">
+        <motion.h1
+          className="text-4xl  text-[#17323D] font-semibold text-center md:text-start"
+          initial={{ translateY: 50, opacity: 0 }}
+          whileInView={{ translateY: 0, opacity: 1 }}
+          transition={{ type: "spring", duration: 0.7, delay: 0.1 }}
+        >
           Leading health care Providers
-        </h1>
-        <p className="my-4 text-center md:text-start ">
+        </motion.h1>
+        <motion.p
+          className="my-4 text-center md:text-start "
+          initial={{ translateY: 50, opacity: 0 }}
+          whileInView={{ translateY: 0, opacity: 1 }}
+          transition={{ type: "spring", duration: 0.7, delay: 0.3 }}
+        >
           We provide the best choices for you. Adjust it to your health needs
           and make sure you get them whenever you need them.
-        </p>
-        <button className="py-2 px-8 border-2 border-[#17323D] text-[#17323D] font-semibold block mx-auto md:inline">
+        </motion.p>
+        <motion.button
+          initial={{ translateY: 50, opacity: 0 }}
+          whileInView={{ translateY: 0, opacity: 1 }}
+          transition={{ type: "spring", duration: 0.7, delay: 0.5 }}
+          className="py-2 px-8 border-2 border-[#17323D] text-[#17323D] font-semibold block mx-auto md:inline"
+        >
           View Products
-        </button>
+        </motion.button>
       </div>
     </div>
   )
