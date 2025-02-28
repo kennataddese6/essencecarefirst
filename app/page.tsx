aef7ac884b138b756423c6d262cf8f14622f5981
kennataddese6 <kennataddese6@gmail.com>
2025-02-28 23:08:22 +0300

    add leads section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Benefits from "@/components/ui/benefits"
 import Featured from "@/components/ui/featured"
 import Hero from "@/components/ui/hero"
+import Lead from "@/components/ui/lead"
 
 export default function Page() {
   return (
@@ -8,6 +9,11 @@ export default function Page() {
       <Hero />
       <Benefits />
       <Featured />
+      <Lead />
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello </h1>
     </>
   )
 }
---
+++ components/ui/lead.tsx
@@ -0,0 +1,20 @@
+const Lead = () => {
+  return (
+    <div className="bg-blue-800 bg-[url(/leads-image.jpg)] h-5/6 bg-no-repeat bg-cover mt-12 flex items-center">
+      <div className="w-1/3 md:mx-20">
+        <h1 className="text-4xl  text-[#17323D] font-semibold">
+          Leading health care Providers
+        </h1>
+        <p className="my-4">
+          We provide the best choices for you. Adjust it to your health needs
+          and make sure you get them whenever you need them.
+        </p>
+        <button className="py-2 px-8 border-2 border-[#17323D] text-[#17323D] font-semibold">
+          View Products
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Lead
