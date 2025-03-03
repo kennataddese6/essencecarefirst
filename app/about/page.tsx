f7468c3d62226f54141f0aa6eee1bd3c158b5c3e
kennataddese6 <kennataddese6@gmail.com>
2025-03-03 22:21:04 +0300

    add resposiveness to about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image"
 
 const About = () => {
   return (
-    <div className="mt-32 mx-20 flex">
-      <div className="w-1/2 p-4">
-        <h1 className="text-3xl font-semibold text-[#447ce4]">About us</h1>
-        <p className="mt-4 pr-12">
+    <div className="mt-16 md:mt-32 md:mx-20 flex flex-wrap">
+      <div className="md:w-1/2 p-4">
+        <h1 className="text-3xl font-semibold text-[#447ce4] text-center md:text-start">
+          About us
+        </h1>
+        <p className="mt-4 mx-1 md:mx-0 md:pr-12 text-center md:text-start">
           We are a dedicated team inspired to provide the best medical health
           care in ethiopia. We strive for excellence and are going to keep up
           with the best techonologies out there. We are a dedicated team
