9933fe608d6998ec31564c42164c9662eb944528
kennataddese6 <kennataddese6@gmail.com>
2025-03-05 19:15:51 +0300

    add loading form adding products

--- app/(admin)/dashboard/products/page.tsx
+++ app/(admin)/dashboard/products/page.tsx
@@ -81,7 +81,7 @@ const Page = () => {
             className="bg-gradient-to-br relative group/btn from-black  to-neutral-600 block  w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] "
             type="submit"
           >
-            Add Product &rarr;
+            {isPending ? "Adding Product " : "Add Product "}
             <BottomGradient />
           </button>
         </form>
