c7f5ece5e19f656a03829b0534c0a2522e43497f
kennataddese6 <kennataddese6@gmail.com>
2025-03-05 16:04:37 +0300

    add table

---
+++ app/(admin)/dashboard/categories/create/page.tsx
@@ -0,0 +1,88 @@
+"use client"
+import { createCategory } from "@/app/action/action"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils"
+import { useActionState, useEffect } from "react"
+import { toast } from "sonner"
+
+const Page = () => {
+  const initialState = {
+    success: false,
+    error: false,
+    errorMessage: "",
+  }
+  const [state, formAction, isPending] = useActionState(
+    createCategory,
+    initialState,
+  )
+  useEffect(() => {
+    if (state.success) {
+      toast.success("created")
+    } else if (state.error) {
+      toast.error(state.errorMessage)
+    }
+  }, [state])
+  return (
+    <div className="w-full h-screen  flex items-center">
+      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4  md:p-8 shadow-input bg-white ">
+        <h2 className="font-bold text-3xl text-neutral-800  text-center">
+          Add Categories
+        </h2>
+        <form className="my-8" action={formAction}>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="name">Category Name</Label>
+            <Input
+              id="name"
+              placeholder="Enter Category Name"
+              type="text"
+              required
+              name="name"
+            />
+          </LabelInputContainer>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="name">Category Description</Label>
+            <Input
+              id="name"
+              placeholder="Enter Category Description"
+              type="text"
+              name="description"
+              required
+            />
+          </LabelInputContainer>
+
+          <button
+            className="bg-gradient-to-br relative group/btn from-black  to-neutral-600 block  w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] "
+            type="submit"
+          >
+            {isPending ? "Adding Category " : "Add Category "}
+            <BottomGradient />
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Page
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode
+  className?: string
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  )
+}
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  )
+}
--- app/(admin)/dashboard/categories/page.tsx
+++ app/(admin)/dashboard/categories/page.tsx
@@ -1,88 +1,70 @@
-"use client"
-import { createCategory } from "@/app/action/action"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { cn } from "@/lib/utils"
-import { useActionState, useEffect } from "react"
-import { toast } from "sonner"
+import Link from "next/link"
 
-const Page = () => {
-  const initialState = {
-    success: false,
-    error: false,
-    errorMessage: "",
-  }
-  const [state, formAction, isPending] = useActionState(
-    createCategory,
-    initialState,
-  )
-  useEffect(() => {
-    if (state.success) {
-      toast.success("created")
-    } else if (state.error) {
-      toast.error(state.errorMessage)
-    }
-  }, [state])
+export default function Page() {
   return (
-    <div className="w-full h-screen  flex items-center">
-      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4  md:p-8 shadow-input bg-white ">
-        <h2 className="font-bold text-3xl text-neutral-800  text-center">
-          Add Categories
-        </h2>
-        <form className="my-8" action={formAction}>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="name">Category Name</Label>
-            <Input
-              id="name"
-              placeholder="Enter Category Name"
-              type="text"
-              required
-              name="name"
-            />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="name">Category Description</Label>
-            <Input
-              id="name"
-              placeholder="Enter Category Description"
-              type="text"
-              name="description"
-              required
-            />
-          </LabelInputContainer>
-
-          <button
-            className="bg-gradient-to-br relative group/btn from-black  to-neutral-600 block  w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] "
-            type="submit"
-          >
-            {isPending ? "Adding Category " : "Add Category "}
-            <BottomGradient />
-          </button>
-        </form>
-      </div>
+    <div className="mx-8 w-full mt-16">
+      <Link href={"/dashboard/categories/create"}>
+        <button className="py-2 px-6 bg-black text-white rounded-lg my-6 block ml-auto shadow-sm shadow-neutral-400">
+          Create
+        </button>
+      </Link>
+      <table className="table-auto border-collapse border border-gray-300 w-full text-left">
+        <thead>
+          <tr className="bg-neutral-400">
+            <th className="border border-gray-300 px-4 py-2">Category Name</th>
+            <th className="border border-gray-300 px-4 py-2">
+              Category Description
+            </th>
+            <th className="border border-gray-300 px-4 py-2"> Category ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 px-4 py-2">
+              Alfreds Futterkiste
+            </td>
+            <td className="border border-gray-300 px-4 py-2">Maria Anders</td>
+            <td className="border border-gray-300 px-4 py-2">Germany</td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 px-4 py-2">
+              Centro comercial Moctezuma
+            </td>
+            <td className="border border-gray-300 px-4 py-2">
+              Francisco Chang
+            </td>
+            <td className="border border-gray-300 px-4 py-2">Mexico</td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 px-4 py-2">Ernst Handel</td>
+            <td className="border border-gray-300 px-4 py-2">Roland Mendel</td>
+            <td className="border border-gray-300 px-4 py-2">Austria</td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 px-4 py-2">Island Trading</td>
+            <td className="border border-gray-300 px-4 py-2">Helen Bennett</td>
+            <td className="border border-gray-300 px-4 py-2">UK</td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 px-4 py-2">
+              Laughing Bacchus Winecellars
+            </td>
+            <td className="border border-gray-300 px-4 py-2">
+              Yoshi Tannamuri
+            </td>
+            <td className="border border-gray-300 px-4 py-2">Canada</td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 px-4 py-2">
+              Magazzini Alimentari Riuniti
+            </td>
+            <td className="border border-gray-300 px-4 py-2">
+              Giovanni Rovelli
+            </td>
+            <td className="border border-gray-300 px-4 py-2">Italy</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
-}
-
-export default Page
-const LabelInputContainer = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode
-  className?: string
-}) => {
-  return (
-    <div className={cn("flex flex-col space-y-2 w-full", className)}>
-      {children}
-    </div>
-  )
-}
-const BottomGradient = () => {
-  return (
-    <>
-      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
-    </>
   )
 }
