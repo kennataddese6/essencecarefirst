6c69b0212b376b1e32db5a2703cd4c6998904ad3
kennataddese6 <kennataddese6@gmail.com>
2025-03-07 23:49:32 +0300

    create dir

--- lib/save-file.tsx
+++ lib/save-file.tsx
@@ -3,20 +3,26 @@ import path from "path"
 
 export async function saveImage(formData: FormData, imageName: string) {
   const image: any = formData.get("image")
-  const imagePath = path.join(
-    process.cwd(),
-    "storage/images",
-    imageName + ".WebP",
-  )
+  const imageDir = path.join(process.cwd(), "storage/images")
+  const imagePath = path.join(imageDir, imageName + ".webp")
   const buffer: any = Buffer.from(await image.arrayBuffer())
 
   return new Promise((resolve, reject) => {
-    fs.writeFile(imagePath, buffer, (err) => {
+    // Ensure the directory exists
+    fs.mkdir(imageDir, { recursive: true }, (err) => {
       if (err) {
-        reject(err)
-      } else {
-        resolve(image.name)
+        reject(err) // Handle directory creation error
+        return
       }
+
+      // Write the image file
+      fs.writeFile(imagePath, buffer, (err) => {
+        if (err) {
+          reject(err) // Handle file write error
+        } else {
+          resolve(image.name) // Successfully saved
+        }
+      })
     })
   })
 }
