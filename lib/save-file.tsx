3ba84365f8cbc89782390ea395a821be031ae7aa
kennataddese6 <kennataddese6@gmail.com>
2025-03-08 00:19:48 +0300

    try tmp

--- lib/save-file.tsx
+++ lib/save-file.tsx
@@ -3,22 +3,20 @@ import path from "path"
 
 export async function saveImage(formData: FormData, imageName: string) {
   const image: any = formData.get("image")
-  const imageDir = path.join(process.cwd(), "tmp/storage/images")
+  const imageDir = path.join("/tmp", "storage/images")
   const imagePath = path.join(imageDir, imageName + ".webp")
   const buffer = Buffer.from(await image.arrayBuffer())
 
-  // Ensure the directory exists synchronously before proceeding
   if (!fs.existsSync(imageDir)) {
     fs.mkdirSync(imageDir, { recursive: true })
   }
 
-  // Save the image file
   return new Promise((resolve, reject) => {
     fs.writeFile(imagePath, buffer, (err) => {
       if (err) {
-        reject(err) // Handle file write error
+        reject(err)
       } else {
-        resolve(image.name) // File successfully saved
+        resolve(image.name)
       }
     })
   })
