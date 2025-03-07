e40f357f7c5886d1aa4db7169734df150d74c581
kennataddese6 <kennataddese6@gmail.com>
2025-03-07 21:25:28 +0300

    add meta data for seo

--- app/(client)/products/[id]/page.tsx
+++ app/(client)/products/[id]/page.tsx
@@ -1,6 +1,18 @@
 import { prisma } from "@/app/lib/prisma"
 import ProductUI from "./product-ui"
 
+export async function generateMetadata({ params }: any) {
+  const productId = Number(params.id)
+  const product = await prisma.product.findUnique({
+    where: { id: productId },
+  })
+
+  return {
+    title: `${product.name} - Essence care first`,
+    description: `${product.description.slice(0, 160)}...`,
+  }
+}
+
 const page = async ({ params }: any) => {
   const param = await params
   const product = await prisma.product.findUnique({
