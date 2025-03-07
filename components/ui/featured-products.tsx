a0d07e14f67cd4a456aa7075dca4c51f1047eda4
kennataddese6 <kennataddese6@gmail.com>
2025-03-07 22:03:52 +0300

    trigger not found if product not found

--- components/ui/featured-products.tsx
+++ components/ui/featured-products.tsx
@@ -1,10 +1,14 @@
 import { prisma } from "@/app/lib/prisma"
+import { notFound } from "next/navigation"
 import Featured from "./featured"
 
 const FeaturedProducts = async () => {
   const products = await prisma.product.findMany({
     where: { category: "Featured" },
   })
+  if (!products) {
+    notFound()
+  }
   return <Featured products={products} />
 }
 
