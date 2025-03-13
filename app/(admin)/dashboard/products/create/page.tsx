a702f9810f1a3b5492ffe0635b8e959301e6158e
kennataddese6 <kennataddese6@gmail.com>
2025-03-13 15:54:47 +0300

    remove cache

--- app/(admin)/dashboard/products/create/page.tsx
+++ app/(admin)/dashboard/products/create/page.tsx
@@ -1,5 +1,8 @@
 import { pool } from "@/app/db"
 import { CreateProductForm } from "./product-create-form"
+
+export const dynamic = "force-dynamic"
+
 const Page = async () => {
   const client = await pool.connect()
   const categoriesRes = await client.query('SELECT * FROM "Category"')
